7a6689e0173970796a42b23127a27ea8af751cb6
junghyunbak <jeong5728@gmail.com>
2024-04-06 11:06:05 +0900

    chore: github actions & docker 설정 파일 추가

---
+++ docker/build-and-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+IMAGE_NAME=issuegram
+
+docker build --no-cache --file "$SCRIPT_DIR/Dockerfile" --tag ${IMAGE_NAME} "$SCRIPT_DIR/.."
+
+docker rm --force ${IMAGE_NAME}
+
+docker image prune --force
+
+docker run --detach --publish 3005:3000 --name ${IMAGE_NAME} ${IMAGE_NAME}
