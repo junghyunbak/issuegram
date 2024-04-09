2cf0570bdecb00668165600d9e8742b0037c1dab
junghyunbak <jeong5728@gmail.com>
2024-04-10 00:00:54 +0900

    chore: 빌드 스크립트 수정

--- docker/build-and-run.sh
+++ docker/build-and-run.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 IMAGE_NAME=issuegram
 
-docker build --no-cache --file "$SCRIPT_DIR/Dockerfile" --tag ${IMAGE_NAME} "$SCRIPT_DIR/.."
-
 docker rm --force ${IMAGE_NAME}
 
-docker image prune --force
+docker build --no-cache --file "$SCRIPT_DIR/Dockerfile" --tag ${IMAGE_NAME} "$SCRIPT_DIR/.."
 
 docker run --detach --publish 3005:3000 --name ${IMAGE_NAME} ${IMAGE_NAME}
+
+docker system prune -af
