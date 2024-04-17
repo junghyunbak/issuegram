dc0d32885a959ed92b563449a16141610fca72b3
junghyunbak <jeong5728@gmail.com>
2024-04-17 21:33:34 +0900

    chore: docker 컨테이너 삭제 스크립트 제거

--- docker/build-and-run.sh
+++ docker/build-and-run.sh
@@ -4,8 +4,6 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 IMAGE_NAME=issuegram
 
-docker rm --force ${IMAGE_NAME}
-
 docker build --no-cache --file "$SCRIPT_DIR/Dockerfile" --tag ${IMAGE_NAME} "$SCRIPT_DIR/.."
 
 docker run --detach --publish 3005:3000 --name ${IMAGE_NAME} ${IMAGE_NAME}
