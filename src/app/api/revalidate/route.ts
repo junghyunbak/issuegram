82a5a475c885a48b28a918b3eae1fd2bd5ae9a10
junghyunbak <jeong5728@gmail.com>
2024-04-21 17:45:29 +0900

    fix: revalidation api 검증 로직 수정

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,12 +1,30 @@
 import { revalidateTag } from "next/cache";
 import { type NextRequest } from "next/server";
 import type { IssueCommentEvent, IssuesEvent } from "@octokit/webhooks-types";
+import * as crypto from "crypto";
+
+const WEBHOOK_SECRET: string = process.env.REVALIDATE_SECRET_TOKEN || "";
+
+/**
+ * https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries#typescript-example
+ */
+const verify_signature = (req: Request) => {
+  const signature = crypto
+    .createHmac("sha256", WEBHOOK_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+
+  let trusted = Buffer.from(`sha256=${signature}`, "ascii");
+  let untrusted = Buffer.from(
+    req.headers.get("x-hub-signature-256") || "",
+    "ascii",
+  );
+
+  return crypto.timingSafeEqual(trusted, untrusted);
+};
 
 export async function POST(request: NextRequest) {
-  if (
-    process.env.REVALIDATE_SECRET_TOKEN !==
-    request.nextUrl.searchParams.get("secret")
-  ) {
+  if (!verify_signature(request)) {
     return Response.json(
       { message: "유효하지 않은 토큰입니다." },
       { status: 401 },
