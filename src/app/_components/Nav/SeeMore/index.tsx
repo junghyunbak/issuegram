40227eb838abeed91c16ccd5c02b21d366c1ffa4
junghyunbak <jeong5728@gmail.com>
2024-04-23 14:43:22 +0900

    style: 더보기 모달 다크모드 스타일링

--- src/app/_components/Nav/SeeMore/index.tsx
+++ src/app/_components/Nav/SeeMore/index.tsx
@@ -58,13 +58,13 @@ export function SeeMore() {
   return (
     <div className="relative mb-[12px]">
       <div
-        className="group flex cursor-pointer rounded-lg p-[12px] hover:bg-black/[0.05] active:opacity-50"
+        className="hover:bg-igHoverOverlay dark:hover:bg-igHoverOverlayDark group flex cursor-pointer rounded-lg p-[12px] active:opacity-50"
         onClick={() => {
           setModalState(modalState === "closed" ? "main" : "closed");
         }}
       >
         <div className="scale-100 transition-[transform] duration-300 group-hover:scale-105">
-          <Hamburger />
+          <Hamburger className="dark:stroke-primaryTextDark stroke-primaryText" />
         </div>
 
         <div className="pl-[16px] max-xl:hidden">더 보기</div>
@@ -73,7 +73,7 @@ export function SeeMore() {
       {modalState !== "closed" && (
         <div
           ref={menuContainer}
-          className="absolute bottom-[52px] left-0 z-[60] h-10 w-[266px] overflow-x-hidden overflow-y-hidden rounded-2xl bg-white shadow-[0_4px_12px_rgba(0,0,0,0.15)] max-xl:bottom-0 max-xl:left-[48px]"
+          className="dark:bg-igBannerBackground absolute bottom-[52px] left-0 z-[60] h-10 w-[266px] overflow-x-hidden overflow-y-hidden rounded-2xl bg-white shadow-[0_4px_12px_rgba(0,0,0,0.15)] max-xl:bottom-0 max-xl:left-[48px]"
         >
           <div
             ref={mainMenu}
@@ -84,9 +84,9 @@ export function SeeMore() {
                 setModalState("mode");
               }}
             >
-              <div className="flex cursor-pointer items-center gap-x-[12px] rounded-lg p-[16px] hover:bg-black/[0.05] active:opacity-50 ">
+              <div className="hover:bg-igHoverOverlay dark:hover:bg-igHoverOverlayDark flex cursor-pointer items-center gap-x-[12px] rounded-lg p-[16px] active:opacity-50 ">
                 {isDark ? <Moon /> : <Sun />}
-                모드 전환
+                <p>모드 전환</p>
               </div>
             </div>
           </div>
@@ -95,7 +95,7 @@ export function SeeMore() {
             className={`absolute top-0 w-full transition-transform ${modalState === "mode" ? "translate-x-0" : " translate-x-full"}`}
           >
             <div>
-              <div className="flex items-center justify-between border-b p-[16px]">
+              <div className="border-igStroke dark:border-igStrokeDark flex items-center justify-between border-b p-[16px]">
                 <div className="flex items-center gap-x-[12px]">
                   <div
                     className="flex h-[24px] w-[24px] cursor-pointer items-center justify-center"
@@ -103,7 +103,7 @@ export function SeeMore() {
                       setModalState("main");
                     }}
                   >
-                    <ArrowUp className="w-[12px] -rotate-90" />
+                    <ArrowUp className="[&_path]:text-igTertiaryIcon w-[12px] -rotate-90" />
                   </div>
                   <p className="font-semibold">모드 전환</p>
                 </div>
@@ -113,13 +113,13 @@ export function SeeMore() {
 
               <div className="p-[8px]">
                 <div
-                  className="flex cursor-pointer items-center justify-between gap-x-[12px] rounded-lg p-[16px] hover:bg-black/[0.05] active:opacity-50"
+                  className="hover:bg-igHoverOverlay dark:hover:bg-igHoverOverlayDark flex cursor-pointer items-center justify-between gap-x-[12px] rounded-lg p-[16px] active:opacity-50"
                   onClick={handleDarkModeToggleButtonClick}
                 >
                   <p className="text-sm">다크 모드</p>
-                  <div className="relative h-[16px] w-[26px] rounded-2xl bg-[#dbdfe4]">
+                  <div className="bg-igToggleBackgroundOnPrism dark:bg-igToggleBackgroundOnPrismDark relative h-[16px] w-[26px] rounded-2xl">
                     <div
-                      className={`absolute inset-[2px] h-[12px] w-[12px] rounded-full bg-white transition-all ${isDark ? "left-[12px]" : ""}`}
+                      className={`bg-igStrokePrism dark:bg-igStrokePrismDark absolute inset-[2px] h-[12px] w-[12px] rounded-full transition-all duration-200 ${isDark ? "left-[12px]" : ""}`}
                     />
                   </div>
                 </div>
