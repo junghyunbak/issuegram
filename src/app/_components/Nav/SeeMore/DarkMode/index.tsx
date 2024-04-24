0655de5fa7fd23d914410518ab2d8cbf289a85c8
junghyunbak <jeong5728@gmail.com>
2024-04-25 02:26:02 +0900

    refactor: 더보기 모달 내 컴포넌트 분리

---
+++ src/app/_components/Nav/SeeMore/DarkMode/index.tsx
@@ -0,0 +1,77 @@
+import { type Dispatch, type SetStateAction, forwardRef } from "react";
+import ArrowUp from "@/assets/svgs/arrow-up.svg";
+import { type ModalState } from "..";
+import Sun from "@/assets/svgs/sun.svg";
+import Moon from "@/assets/svgs/moon.svg";
+import useStore from "@/store";
+
+interface DarkModeProps {
+  modalState: ModalState;
+  setModalState: Dispatch<SetStateAction<ModalState>>;
+}
+
+const DarkMode = forwardRef<HTMLDivElement | null, DarkModeProps>(
+  function _DarkMode({ modalState, setModalState }, ref) {
+    const [isDark, setIsDark] = useStore((state) => [
+      state.isDark,
+      state.setIsDark,
+    ]);
+
+    const handleDarkModeToggleButtonClick = () => {
+      if (!isDark) {
+        setIsDark(true);
+
+        window.localStorage.setItem("theme", "dark");
+
+        document.documentElement.classList.add("dark");
+      } else {
+        setIsDark(false);
+
+        window.localStorage.removeItem("theme");
+
+        document.documentElement.classList.remove("dark");
+      }
+    };
+
+    return (
+      <div
+        ref={ref}
+        className={`absolute top-0 w-full transition-transform ${modalState === "mode" ? "translate-x-0" : " translate-x-full"}`}
+      >
+        <div>
+          <div className="flex items-center justify-between border-b border-igStroke p-[16px] dark:border-igStrokeDark">
+            <div className="flex items-center gap-x-[12px]">
+              <div
+                className="flex h-[24px] w-[24px] cursor-pointer items-center justify-center"
+                onClick={() => {
+                  setModalState("main");
+                }}
+              >
+                <ArrowUp className="w-[12px] -rotate-90 text-igTertiaryIcon" />
+              </div>
+              <p className="font-semibold">모드 전환</p>
+            </div>
+
+            {isDark ? <Moon /> : <Sun />}
+          </div>
+
+          <div className="p-[8px]">
+            <div
+              className="flex cursor-pointer items-center justify-between gap-x-[12px] rounded-lg p-[16px] hover:bg-igHoverOverlay active:opacity-50 dark:hover:bg-igHoverOverlayDark"
+              onClick={handleDarkModeToggleButtonClick}
+            >
+              <p className="text-sm">다크 모드</p>
+              <div className="relative h-[16px] w-[26px] rounded-2xl bg-igToggleBackgroundOnPrism dark:bg-igToggleBackgroundOnPrismDark">
+                <div
+                  className={`absolute inset-[2px] h-[12px] w-[12px] rounded-full bg-igStrokePrism transition-all duration-200 dark:bg-igStrokePrismDark ${isDark ? "left-[12px]" : ""}`}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+);
+
+export { DarkMode };
--- src/app/_components/Nav/SeeMore/index.tsx
+++ src/app/_components/Nav/SeeMore/index.tsx
@@ -1,28 +1,25 @@
 "use client";
 
-import Hamburger from "@/assets/svgs/hamburger.svg";
-import Sun from "@/assets/svgs/sun.svg";
-import Moon from "@/assets/svgs/moon.svg";
-import ArrowUp from "@/assets/svgs/arrow-up.svg";
 import { useEffect, useRef, useState } from "react";
+import { DarkMode } from "./DarkMode";
+import { MainMenu } from "./MainMenu";
 import useStore from "@/store";
+import Hamburger from "@/assets/svgs/hamburger.svg";
 
-// [ ]: 다크모드 관련 컴포넌트 분리
+export type ModalState = "closed" | "main" | "mode";
+
 export function SeeMore() {
-  const [modalState, setModalState] = useState<"closed" | "main" | "mode">(
-    "closed",
-  );
-
-  const [isDark, setIsDark] = useStore((state) => [
-    state.isDark,
-    state.setIsDark,
-  ]);
+  const [modalState, setModalState] = useState<ModalState>("closed");
+  const [setIsDark] = useStore((state) => [state.setIsDark]);
 
   const menuContainer = useRef<HTMLDivElement | null>(null);
   const mainMenu = useRef<HTMLDivElement | null>(null);
   const modeMenu = useRef<HTMLDivElement | null>(null);
   const modalToggleButton = useRef<HTMLDivElement | null>(null);
 
+  /**
+   * 메뉴 종류에 따라 모달 높이 수정
+   */
   useEffect(() => {
     if (
       modalState === "closed" ||
@@ -36,14 +33,9 @@ export function SeeMore() {
     menuContainer.current.style.height = `${modalState === "main" ? mainMenu.current.offsetHeight : modeMenu.current.offsetHeight}px`;
   }, [modalState]);
 
-  useEffect(() => {
-    if (window.localStorage.getItem("theme") === "dark") {
-      setIsDark(true);
-
-      document.documentElement.classList.add("dark");
-    }
-  }, []);
-
+  /**
+   * 모달 외부 클릭 및 esc키 누를 경우 모달 닫기
+   */
   useEffect(() => {
     const handleCloseModal = (e: MouseEvent) => {
       if (
@@ -77,24 +69,19 @@ export function SeeMore() {
     };
   }, []);
 
-  const handleModalToggleButtonClick = () => {
-    setModalState(modalState === "closed" ? "main" : "closed");
-  };
-
-  const handleDarkModeToggleButtonClick = () => {
-    if (!isDark) {
+  /**
+   * localstorage에 저장된 테마에 따라 다크모드 설정
+   */
+  useEffect(() => {
+    if (window.localStorage.getItem("theme") === "dark") {
       setIsDark(true);
 
-      window.localStorage.setItem("theme", "dark");
-
       document.documentElement.classList.add("dark");
-    } else {
-      setIsDark(false);
-
-      window.localStorage.removeItem("theme");
-
-      document.documentElement.classList.remove("dark");
     }
+  }, []);
+
+  const handleModalToggleButtonClick = () => {
+    setModalState(modalState === "closed" ? "main" : "closed");
   };
 
   return (
@@ -118,57 +105,17 @@ export function SeeMore() {
           ref={menuContainer}
           className="absolute bottom-[52px] left-0 z-10 h-10 w-[266px] overflow-x-hidden overflow-y-hidden rounded-2xl bg-white shadow-[0_4px_12px_rgba(0,0,0,0.15)] max-xl:bottom-0 max-xl:left-[48px] dark:bg-igBannerBackgroundDark"
         >
-          <div
+          <MainMenu
             ref={mainMenu}
-            className={`absolute top-0 w-full p-[8px] transition-transform ${modalState === "main" ? "" : "-translate-x-full"}`}
-          >
-            <div
-              onClick={() => {
-                setModalState("mode");
-              }}
-            >
-              <div className="flex cursor-pointer items-center gap-x-[12px] rounded-lg p-[16px] hover:bg-igHoverOverlay active:opacity-50 dark:hover:bg-igHoverOverlayDark ">
-                {isDark ? <Moon /> : <Sun />}
-                <p>모드 전환</p>
-              </div>
-            </div>
-          </div>
-          <div
+            modalState={modalState}
+            setModalState={setModalState}
+          />
+
+          <DarkMode
             ref={modeMenu}
-            className={`absolute top-0 w-full transition-transform ${modalState === "mode" ? "translate-x-0" : " translate-x-full"}`}
-          >
-            <div>
-              <div className="flex items-center justify-between border-b border-igStroke p-[16px] dark:border-igStrokeDark">
-                <div className="flex items-center gap-x-[12px]">
-                  <div
-                    className="flex h-[24px] w-[24px] cursor-pointer items-center justify-center"
-                    onClick={() => {
-                      setModalState("main");
-                    }}
-                  >
-                    <ArrowUp className="w-[12px] -rotate-90 text-igTertiaryIcon" />
-                  </div>
-                  <p className="font-semibold">모드 전환</p>
-                </div>
-
-                {isDark ? <Moon /> : <Sun />}
-              </div>
-
-              <div className="p-[8px]">
-                <div
-                  className="flex cursor-pointer items-center justify-between gap-x-[12px] rounded-lg p-[16px] hover:bg-igHoverOverlay active:opacity-50 dark:hover:bg-igHoverOverlayDark"
-                  onClick={handleDarkModeToggleButtonClick}
-                >
-                  <p className="text-sm">다크 모드</p>
-                  <div className="relative h-[16px] w-[26px] rounded-2xl bg-igToggleBackgroundOnPrism dark:bg-igToggleBackgroundOnPrismDark">
-                    <div
-                      className={`absolute inset-[2px] h-[12px] w-[12px] rounded-full bg-igStrokePrism transition-all duration-200 dark:bg-igStrokePrismDark ${isDark ? "left-[12px]" : ""}`}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            modalState={modalState}
+            setModalState={setModalState}
+          />
         </div>
       )}
     </div>
