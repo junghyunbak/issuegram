178b5011040c7b3903a0c4c836fdb441662a4161
junghyunbak <jeong5728@gmail.com>
2024-04-25 14:11:03 +0900

    refactor: 이슈 모달 헤더 메뉴 모달 컴포넌트 분리

---
+++ src/app/issue/[number]/_components/IssueHeader/IssueHeaderMenu/IssueHeaderMenuModal/index.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import type { Dispatch, SetStateAction } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import config from "@/config";
+import useStore from "@/store";
+import "./index.css";
+
+interface IssueHeaderMenuModalProps {
+  issue: Issues[number];
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export function IssueHeaderMenuModal({
+  issue,
+  setIsModalOpen,
+}: IssueHeaderMenuModalProps) {
+  const fireCopiedSnackbar = useStore((state) => state.fireCopiedSnackbar);
+
+  const handleDimmedClick = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="issue-header-modal">
+      <div
+        className="absolute size-full bg-black/65"
+        onClick={handleDimmedClick}
+      />
+
+      <div className="absolute w-[calc(100dvw-88px)] max-w-[400px] overflow-hidden rounded-xl bg-white">
+        <ul className="issue-header-modal-list">
+          <li>
+            <a
+              className="flex size-full items-center justify-center"
+              href={issue.html_url}
+            >
+              수정
+            </a>
+          </li>
+
+          <li>
+            <a
+              className="flex size-full items-center justify-center"
+              href={`/issue/${issue.number}`}
+            >
+              게시물로 이동
+            </a>
+          </li>
+
+          <li>
+            <CopyToClipboard
+              text={`https://${config.domain}/issue/${issue.number}`}
+            >
+              <div
+                className="flex size-full items-center justify-center"
+                onClick={() => {
+                  setIsModalOpen(false);
+                  fireCopiedSnackbar();
+                }}
+              >
+                <p>링크 복사</p>
+              </div>
+            </CopyToClipboard>
+          </li>
+
+          <li>
+            <div
+              className="flex size-full items-center justify-center"
+              onClick={() => setIsModalOpen(false)}
+            >
+              <p>취소</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/app/issue/[number]/_components/IssueHeader/IssueHeaderMenu/index.tsx
+++ src/app/issue/[number]/_components/IssueHeader/IssueHeaderMenu/index.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import Kebab from "@/assets/svgs/kebab.svg";
 import { useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import useStore from "@/store";
-import config from "@/config";
-import "./index.css";
+import { IssueHeaderMenuModal } from "./IssueHeaderMenuModal";
+import Kebab from "@/assets/svgs/kebab.svg";
 
 interface IssueHeaderMenuProps {
   issue: Issues[number];
@@ -14,75 +11,23 @@ interface IssueHeaderMenuProps {
 export function IssueHeaderMenu({ issue }: IssueHeaderMenuProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const fireCopiedSnackbar = useStore((state) => state.fireCopiedSnackbar);
+  const handleMenuOpenButtonClick = () => {
+    setIsModalOpen(true);
+  };
 
   return (
     <>
       <div className="flex items-center justify-center">
         <div
           className="group cursor-pointer p-[8px]"
-          onClick={() => setIsModalOpen(true)}
+          onClick={handleMenuOpenButtonClick}
         >
           <Kebab className="group-hover:text-igSecondaryText group-active:opacity-50 dark:group-hover:text-igSecondaryTextDark" />
         </div>
       </div>
 
       {isModalOpen && (
-        <div className="issue-header-modal">
-          <div
-            className="absolute size-full bg-black/65"
-            onClick={() => {
-              setIsModalOpen(false);
-            }}
-          />
-
-          <div className="absolute w-[calc(100dvw-88px)] max-w-[400px] overflow-hidden rounded-xl bg-white">
-            <ul className="issue-header-modal-list">
-              <li>
-                <a
-                  className="flex size-full items-center justify-center"
-                  href={issue.html_url}
-                >
-                  수정
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="flex size-full items-center justify-center"
-                  href={`/issue/${issue.number}`}
-                >
-                  게시물로 이동
-                </a>
-              </li>
-
-              <li>
-                <CopyToClipboard
-                  text={`https://${config.domain}/issue/${issue.number}`}
-                >
-                  <div
-                    className="flex size-full items-center justify-center"
-                    onClick={() => {
-                      setIsModalOpen(false);
-                      fireCopiedSnackbar();
-                    }}
-                  >
-                    <p>링크 복사</p>
-                  </div>
-                </CopyToClipboard>
-              </li>
-
-              <li>
-                <div
-                  className="flex size-full items-center justify-center"
-                  onClick={() => setIsModalOpen(false)}
-                >
-                  <p>취소</p>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <IssueHeaderMenuModal issue={issue} setIsModalOpen={setIsModalOpen} />
       )}
     </>
   );
