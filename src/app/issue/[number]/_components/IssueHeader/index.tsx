8e77e35c87109417487f8b1fd72317b07e7ea3cd
junghyunbak <jeong5728@gmail.com>
2024-09-03 15:24:29 +0900

    style: 이슈 헤더 말줄임 처리

--- src/app/issue/[number]/_components/IssueHeader/index.tsx
+++ src/app/issue/[number]/_components/IssueHeader/index.tsx
@@ -4,26 +4,26 @@ interface IssueHeaderProps {
   issue: Issues[number];
 }
 
-// [ ]: 넘치는 title 말줄임 처리할 것
-
 export function IssueHeader({ issue }: IssueHeaderProps) {
   return (
-    <div className="border-igSeparator dark:border-igSeparatorDark flex justify-between border-b p-[14px] pr-[8px]">
-      <div className="flex flex-1 items-center overflow-hidden">
-        <img
-          className="h-[32px] w-[32px] rounded-full"
-          src={issue.user ? issue.user.avatar_url : ""}
-        />
+    <div className="flex w-full border-b border-igSeparator p-[14px] pr-[8px] dark:border-igSeparatorDark">
+      <div className="flex w-full justify-between">
+        <div className="flex items-center gap-[14px] overflow-hidden">
+          <img
+            className="h-[32px] w-[32px] rounded-full"
+            src={issue.user ? issue.user.avatar_url : ""}
+          />
 
-        <div className="ml-[14px]">
-          <p className="text-sm font-semibold">
-            {issue.user ? issue.user.login : ""}
-          </p>
-          <p className="whitespace-nowrap text-xs">{issue.title}</p>
+          <div className="flex-1 overflow-hidden">
+            <p className="truncate text-sm font-semibold">
+              {issue.user ? issue.user.login : ""}
+            </p>
+            <p className="truncate whitespace-nowrap text-xs">{issue.title}</p>
+          </div>
         </div>
-      </div>
 
-      <IssueHeaderMenu issue={issue} />
+        <IssueHeaderMenu issue={issue} />
+      </div>
     </div>
   );
 }
