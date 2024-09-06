5f0773dd9e8834453b45957763381d946eee23a8
junghyunbak <jeong5728@gmail.com>
2024-09-06 14:22:01 +0900

    style: dom 구조 최적화 (flex 속성 제대로 활용)

--- src/app/issue/[...slug]/_components/IssueHeader/index.tsx
+++ src/app/issue/[...slug]/_components/IssueHeader/index.tsx
@@ -7,23 +7,21 @@ interface IssueHeaderProps {
 export function IssueHeader({ issue }: IssueHeaderProps) {
   return (
     <div className="flex w-full border-b border-igSeparator p-[14px] pr-[8px] dark:border-igSeparatorDark">
-      <div className="flex w-full justify-between">
-        <div className="flex items-center gap-[14px] overflow-hidden">
-          <img
-            className="h-[32px] w-[32px] rounded-full"
-            src={issue.user ? issue.user.avatar_url : ""}
-          />
+      <div className="flex flex-grow items-center gap-[14px] overflow-hidden">
+        <img
+          className="h-[32px] w-[32px] rounded-full"
+          src={issue.user ? issue.user.avatar_url : ""}
+        />
 
-          <div className="flex-1 overflow-hidden">
-            <p className="truncate text-sm font-semibold">
-              {issue.user ? issue.user.login : ""}
-            </p>
-            <p className="truncate whitespace-nowrap text-xs">{issue.title}</p>
-          </div>
+        <div className="flex-grow overflow-hidden">
+          <p className="truncate text-sm font-semibold">
+            {issue.user ? issue.user.login : ""}
+          </p>
+          <p className="truncate whitespace-nowrap text-xs">{issue.title}</p>
         </div>
-
-        <IssueHeaderMenu issue={issue} />
       </div>
+
+      <IssueHeaderMenu issue={issue} />
     </div>
   );
 }
